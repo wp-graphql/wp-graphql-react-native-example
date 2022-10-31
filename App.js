37c911e0e82e89ef61fcbb80724f70dfbd80c070
Jason Bahl <jasonbahl@mac.com>
2022-10-31 14:38:57 -0600

    - initial commit

---
+++ App.js
@@ -0,0 +1,244 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  useQuery,
+} from '@apollo/client';
+import RenderHtml from 'react-native-render-html';
+
+import type {Node} from 'react';
+import {
+  ImageBackground,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  useWindowDimensions,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import HermesBadge from 'react-native/Libraries/NewAppScreen/components/HermesBadge';
+
+const Header = (): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <ImageBackground
+      accessibilityRole="image"
+      testID="new-app-screen-header"
+      source={require('./logo.png')}
+      style={[
+        styles.background,
+        {
+          backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        },
+      ]}
+      imageStyle={styles.logo}>
+      <HermesBadge />
+      <Text
+        style={[
+          styles.text,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        Welcome to
+        {'\n'}
+        WPGraphQL Native
+      </Text>
+    </ImageBackground>
+  );
+};
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'https://content.wpgraphql.com/graphql',
+  cache: new InMemoryCache(),
+});
+
+const GET_POSTS = gql`
+  query GET_POSTS {
+    posts {
+      nodes {
+        id
+        title
+        uri
+        excerpt
+      }
+    }
+  }
+`;
+
+const Home: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const { width } = useWindowDimensions();
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const {data, loading, error} = useQuery(GET_POSTS);
+
+  if (loading) {
+    return (
+      <SafeAreaView style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Text>Loading...</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="WPGraphQL in Native iOS Apps">
+            This is an example application showing how to use WPGraphQL to
+            populate a native iOS App
+          </Section>
+          <Section
+            title="Posts"
+            style={{
+              marginBottom: 20,
+            }}>
+            {data?.posts?.nodes.map(post => (
+              <Section key={post?.id} title={post?.title}>
+                <RenderHtml
+                  contentWidth={width}
+                  source={{html: post?.excerpt}}
+                />
+              </Section>
+            ))}
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const App: () => Node = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Home />
+    </ApolloProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  background: {
+    paddingBottom: 40,
+    paddingTop: 96,
+    paddingHorizontal: 32,
+  },
+  logo: {
+    opacity: 0.2,
+    overflow: 'visible',
+    resizeMode: 'cover',
+    /*
+     * These negative margins allow the image to be offset similarly across screen sizes and component sizes.
+     *
+     * The source logo.png image is 512x512px, so as such, these margins attempt to be relative to the
+     * source image's size.
+     */
+    marginLeft: -128,
+    marginBottom: -192,
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  postTitle: {
+    fontSize: 20,
+    fontWeight: '300',
+    marginTop: 8,
+  },
+});
+
+export default App;
